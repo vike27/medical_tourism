5d70a1a5fa2100703e1b4bdb95abcf28f59a3c6c
vike27 <norisjavier@gmail.com>
2016-11-23 03:38:38 -0500

    adding message response

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+def create
+  @user = User.new(user_params)
+  @user.save
+  redirect_to root_path, notice: 'Your request has been processed. One of our team members will be reaching out to you shortly.' 
+end
+
+private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+
+end
