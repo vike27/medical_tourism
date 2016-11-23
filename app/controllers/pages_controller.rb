0493a3509c214e88d8bf6b67a877840987b604ac
vike27 <norisjavier@gmail.com>
2016-11-23 02:52:19 -0500

    making some copy changes;

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
+    @user = User.new
   end
+
+private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+
 end
