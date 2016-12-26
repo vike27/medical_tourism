a488f552905ad59fa73f5b9a00ee64b5287b54fd
vike27 <norisjavier@gmail.com>
2016-12-26 02:35:52 -0500

    making changes to website

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,7 +15,7 @@ class PagesController < ApplicationController
 private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
+    params.require(:user).permit(:first_name, :last_name, :email, :cimavax, :senolytics)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ end
 private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
+    params.require(:user).permit(:first_name, :last_name, :email, :cimavax, :senolytics)
   end
 
 end
